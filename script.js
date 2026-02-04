6b4dafe9132242eb950a0a3e98b5aa6192369b4d
jakubturek947 <jakub.turek002@gmail.com>
2026-02-04 09:50:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const viewer = pannellum.viewer('panorama', {
+  "default": {
+    "firstScene": "front",
+    "sceneFadeDuration": 1200,
+    "autoLoad": true,
+    "showControls": false,
+    "hfov": 80, // Větší přiblížení (původně 100)
+    "minHfov": 50, // Maximální dovolený zoom dovnitř
+    "maxHfov": 110  // Maximální dovolený zoom ven
+  },
+
+  "scenes": {
+    "front": {
+      "title": "Zepředu",
+      "type": "equirectangular",
+      "panorama": "data/front.jpg"
+    },
+    "side": {
+      "title": "Z boku",
+      "type": "equirectangular",
+      "panorama": "data/side.jpg"
+    },
+    "back": {
+      "title": "Zezadu",
+      "type": "equirectangular",
+      "panorama": "data/back.jpg"
+    },
+    "top": {
+      "title": "Zhora",
+      "type": "equirectangular",
+      "panorama": "data/top.jpg"
+    }
+  }
+});
+
+function updateTitle() {
+  const sceneId = viewer.getScene();
+  if (sceneId && viewer.getConfig().scenes[sceneId]) {
+    document.getElementById('sceneTitle').innerText = viewer.getConfig().scenes[sceneId].title;
+  }
+}
+
+viewer.on('load', updateTitle);
